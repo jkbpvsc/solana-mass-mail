fd8f805427ff77fc87216f9422c535761132c98c
J <pvsc@proton.me>
2024-06-05 12:34:55 +0200

    feat: nonblocking api

--- src/lib.rs
+++ src/lib.rs
@@ -254,6 +254,170 @@ pub fn massmail<T: Signers + Clone>(
     Ok(())
 }
 
+pub mod nonblocking {
+    use super::*;
+    use solana_client::nonblocking::rpc_client::RpcClient;
+
+    pub async fn dryrun<T: Signers + Clone>(
+        rpc_client: &RpcClient,
+        tx_builders: Vec<MailBuilder<T>>,
+    ) -> Result<(), MassmailError> {
+        let start = Instant::now();
+        let total_to_send = tx_builders.len();
+
+        let blockhash = rpc_client.get_latest_blockhash().await?;
+
+        for tx_builder in tx_builders {
+            let tx = tx_builder.make_signed_versioned_transaction(blockhash)?;
+
+            match rpc_client.simulate_transaction(&tx).await {
+                Ok(res) => {
+                    debug!("Simulated transaction: {:?}", res.value);
+
+                    if let Some(e) = res.value.err {
+                        error!("Transaction failed: {} {:#?}", e, res.value.logs);
+                    }
+                }
+                Err(e) => {
+                    error!("Failed to simulate transaction: {:?}", e);
+                }
+            }
+        }
+
+        info!(
+            "All ({}) transactions dry run, took: {:?}",
+            total_to_send,
+            start.elapsed()
+        );
+
+        Ok(())
+    }
+
+    pub async fn massmail<T: Signers + Clone>(
+        rpc_client: &RpcClient,
+        mut outstanding_to_send: Vec<MailBuilder<T>>,
+        cfg: MassmailCfg,
+    ) -> Result<(), MassmailError> {
+        if cfg.dry_run {
+            return dryrun(rpc_client, outstanding_to_send).await;
+        }
+
+        let start = Instant::now();
+        let mut to_send_next = vec![];
+        let mut to_confirm = HashMap::new();
+        let mut landed = HashSet::new();
+
+        let mut blockhash;
+        let total_to_send = outstanding_to_send.len();
+
+        let mut failures = 0;
+
+        while landed.len() != total_to_send {
+            // Fill up the to_send_next
+            while to_send_next.len() < (cfg.max_at_once - to_confirm.len())
+                && !outstanding_to_send.is_empty()
+                && to_confirm.len() < cfg.max_at_once
+            {
+                to_send_next.push(outstanding_to_send.pop().unwrap());
+            }
+
+            debug!("Sending {} transactions", to_send_next.len());
+
+            blockhash = rpc_client.get_latest_blockhash().await.unwrap();
+
+            while let Some(tx_builder) = to_send_next.pop() {
+                let tx = tx_builder.make_signed_versioned_transaction(blockhash)?;
+                let sig = rpc_client
+                    .send_transaction_with_config(
+                        &tx,
+                        RpcSendTransactionConfig {
+                            skip_preflight: cfg.skip_preflight,
+                            ..Default::default()
+                        },
+                    )
+                    .await;
+
+                match sig {
+                    Ok(sig) => {
+                        debug!("Sent transaction: {:?}", sig);
+
+                        to_confirm.insert(sig, (tx_builder, blockhash, Instant::now()));
+                    }
+                    Err(e) => {
+                        failures += 1;
+
+                        if failures > cfg.max_failures {
+                            error!("Too many failures, exiting");
+                            return Err(MassmailError::TooManyFailures);
+                        }
+
+                        outstanding_to_send.push(tx_builder.clone());
+                        error!("Failed to send transaction: {:?}", e);
+                    }
+                }
+            }
+
+            // Confirm transactions
+            let mut to_remove_from_to_confirm = vec![];
+            for (sig, (tx_builder, blockhash, send_time)) in to_confirm.iter() {
+                debug!("Checking transaction: {:?}", sig);
+                let status = rpc_client
+                    .get_signature_status_with_commitment(sig, cfg.commitment)
+                    .await;
+
+                match status {
+                    Ok(None) => {
+                        // Transaction not confirmed yet
+                        let blockhash_not_found = rpc_client
+                            .is_blockhash_valid(blockhash, cfg.commitment)
+                            .await?;
+
+                        if !blockhash_not_found && send_time.elapsed() >= cfg.tx_timeout {
+                            debug!("Blockhash expired: {:?}", sig);
+
+                            to_remove_from_to_confirm.push(*sig);
+                            outstanding_to_send.push(tx_builder.clone());
+                        }
+                    }
+                    Ok(Some(status)) => {
+                        debug!("Transaction landed: {:?}, success: {}", sig, status.is_ok());
+                        landed.insert(*sig);
+                        to_remove_from_to_confirm.push(*sig);
+
+                        if let Err(e) = status {
+                            error!("Transaction failed: {:?}", e);
+                        }
+                    }
+                    Err(e) => {
+                        debug!("Failed to get signature status: {:?}", e);
+                    }
+                }
+            }
+
+            for sig in to_remove_from_to_confirm {
+                to_confirm.remove(&sig);
+            }
+
+            sleep(Duration::from_millis(500));
+
+            debug!(
+                "Landed: {}/{}, to_confirm: {}",
+                landed.len(),
+                total_to_send,
+                to_confirm.len(),
+            );
+        }
+
+        info!(
+            "All ({}) transactions landed, took: {:?}",
+            total_to_send,
+            start.elapsed()
+        );
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::sync::Arc;
